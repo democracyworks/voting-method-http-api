2c26770aa23bd834d08b781de1641d1ccbac1d0b
Wes Morgan <wesmorgan@icloud.com>
2017-04-15 19:22:14 -0600

    Update deps

--- project.clj
+++ project.clj
@@ -5,27 +5,27 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [turbovote.resource-config "0.2.0"]
+                 [turbovote.resource-config "0.2.1"]
                  [com.novemberain/langohr "3.7.0"]
-                 [prismatic/schema "1.1.3"]
-                 [ch.qos.logback/logback-classic "1.1.8"]
+                 [prismatic/schema "1.1.5"]
+                 [ch.qos.logback/logback-classic "1.2.3"]
 
                  ;; core.async has to come before pedestal or kehaar.wire-up will
                  ;; not compile. Something to do with the try-catch in
                  ;; kehaar.core/go-handler. (This may not be true anymore in
                  ;; core.async 0.2.x; need to test.)
-                 [org.clojure/core.async "0.2.395"]
-                 [democracyworks/kehaar "0.8.1"]
+                 [org.clojure/core.async "0.3.442"]
+                 [democracyworks/kehaar "0.10.1"]
 
-                 [io.pedestal/pedestal.service "0.4.1"]
-                 [io.pedestal/pedestal.service-tools "0.4.1"]
+                 [io.pedestal/pedestal.service "0.5.2"]
+                 [io.pedestal/pedestal.service-tools "0.5.2"]
                  [democracyworks/pedestal-toolbox "0.7.0"]
-                 [org.immutant/web "2.1.5"]
-                 [io.pedestal/pedestal.immutant "0.4.1"]
-                 [org.immutant/core "2.1.5"]
-                 [democracyworks/bifrost "0.1.4"]]
+                 [org.immutant/web "2.1.6"]
+                 [io.pedestal/pedestal.immutant "0.5.2"]
+                 [org.immutant/core "2.1.6"]
+                 [democracyworks/bifrost "0.1.5"]]
   :plugins [[lein-immutant "2.1.0"]
-            [com.carouselapps/jar-copier "0.3.0"]]
+            [com.carouselapps/jar-copier "0.3.1"]]
   :java-agents [[com.newrelic.agent.java/newrelic-agent "3.35.1"]]
   :jar-copier {:java-agents true
                :destination "resources/jars"}
@@ -34,5 +34,5 @@
   :uberjar-name "voting-method-http-api.jar"
   :profiles {:uberjar {:aot :all}
              :dev {:resource-paths ["dev-resources"]}
-             :test {:dependencies [[clj-http "3.4.1"]]
+             :test {:dependencies [[clj-http "3.5.0"]]
                     :jvm-opts ["-Dlog-level=OFF"]}})
